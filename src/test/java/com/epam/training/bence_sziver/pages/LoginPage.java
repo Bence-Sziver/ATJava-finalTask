2139cad2f0555b861473f7a10ae80b07748f6bd2
Bence Sziver <sziver98@gmail.com>
2025-11-17 16:51:41 +0100

    Create LoginPage POM with basic structure

---
+++ src/test/java/com/epam/training/bence_sziver/pages/LoginPage.java
@@ -0,0 +1,25 @@
+package com.epam.training.bence_sziver.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+    private WebDriver driver;
+    private static final String url = "https://www.saucedemo.com/";
+
+    @FindBy(xpath = "//input[@id='user-name']")
+    private WebElement usernameField;
+
+    @FindBy(xpath = "//input[@id='password']")
+    private WebElement passwordField;
+
+    @FindBy(xpath = "//input[@id='login-button']")
+    private WebElement loginButton;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+}
