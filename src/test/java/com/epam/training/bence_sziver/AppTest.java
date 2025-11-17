17cedfd7fb03cc8815a0010f94c5bdb0d3ea09da
Bence Sziver <sziver98@gmail.com>
2025-11-17 14:47:32 +0100

    create maven project and add dependencies

---
+++ src/test/java/com/epam/training/bence_sziver/AppTest.java
@@ -0,0 +1,19 @@
+package com.epam.training.bence_sziver;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
