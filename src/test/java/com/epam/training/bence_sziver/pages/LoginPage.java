c6abf60cdf0903e6a081d364943e2a0be4fee0a2
Bence Sziver <sziver98@gmail.com>
2025-11-17 17:28:55 +0100

    Create POM methods for logging in

--- src/test/java/com/epam/training/bence_sziver/pages/LoginPage.java
+++ src/test/java/com/epam/training/bence_sziver/pages/LoginPage.java
@@ -1,9 +1,7 @@
 package com.epam.training.bence_sziver.pages;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
@@ -26,5 +24,15 @@ public class LoginPage {
     public LoginPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
+    }
+
+    public void openPage() {
+        driver.get(url);
+    }
+
+    public void login(String username, String password) {
+        usernameField.sendKeys(username);
+        passwordField.sendKeys(password);
+        loginButton.click();
     }
 }
